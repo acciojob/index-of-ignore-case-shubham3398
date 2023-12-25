f553d75d90e39c4443c8cdecc15af4ad4d5cf1d8
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-25 15:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function indexOfIgnoreCase(s1, s2) {
 
 		e++;
 	}
+	return -1;
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2)); 
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2)); 
