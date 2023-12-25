15f008ec29a0a2d544f3433babada8f30116d0e6
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-25 15:14:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let e = s2.length;
+
+	while(e <= s1.length){
+		let s = e - s2.length;
+
+		let word = s1.substring(s, e);
+		if(word.toLowerCase() === s2.toLowerCase()){
+			return s;
+		}
+
+		e++;
+	}
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+alert(indexOfIgnoreCase(s1, s2)); 
